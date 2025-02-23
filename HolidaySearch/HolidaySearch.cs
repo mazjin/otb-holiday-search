48a5c088fe144f6448e15ce4a4d77aeeb1718b1e
mazjin <mazjin55555@gmail.com>
2025-02-23 13:48:53 +0000

    add functionality to fetch correct flights for returned hotels 🟢 (GREEN)

--- HolidaySearch/HolidaySearch.cs
+++ HolidaySearch/HolidaySearch.cs
@@ -10,9 +10,9 @@ public class HolidaySearch
     {
         var hotels = hotelsSearch.GetHotels();
         var flights = flightsSearch.GetFlights();
-        Results = hotels.Take(5).Select((hotel, index) => new HolidaySearchResult
+        Results = hotels.Take(5).SelectMany(hotel => hotel.LocalAirports, (hotel, airport) => new HolidaySearchResult
         {
-            Flight = flights[index],
+            Flight = flights.FirstOrDefault(x => x.To == airport),
             Hotel = hotel,
         }).OrderBy(x => x.TotalPrice).ToList();
     }
